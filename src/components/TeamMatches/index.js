5edfdf791bfe3bb9aebba61381c233e0d69c4bb5
Your Name <reddyprashanth512@gmail.com>
2023-12-11 11:59:36 +0000

    solution

--- src/components/TeamMatches/index.js
+++ src/components/TeamMatches/index.js
@@ -24,22 +24,49 @@ class TeamMatches extends Component {
     this.setState({teamsMatch: updatedTeams})
   }
 
+  getGradientColor = id => {
+    switch (id) {
+      case 'team1':
+        return ['#d91c1f', '#1e293b']
+      case 'team2':
+        return ['#4f5db0', '#5755a7']
+
+      case 'team3':
+        return ['#d91c1f', '#a4261d']
+      case 'team4':
+        return ['#f7db00', '#ffffff33']
+      case 'team5':
+        return ['#da237b', ' #ffffff33']
+      case 'team6':
+        return [' #13418b', '#5755a7']
+      case 'team7':
+        return ['#f26d22', '#ffffff33']
+      case 'team8':
+        return ['#5755a7', '#4f5db0']
+      default:
+        return ['white', 'white']
+    }
+  }
+
   renderTeamsMatches = () => {
     const {teamsMatch} = this.state
-    const {teamBannerUrl} = teamsMatch
+    const {teamBannerUrl, teamColors} = teamsMatch
+    const gradientColors =
+      teamColors && teamColors.length ? teamColors.join(', ') : 'white, white'
+
+    const containerStyle = {
+      background: `linear-gradient(to right, ${gradientColors})`,
+      padding: '20px',
+    }
     return (
-      <div className="team-color-container">
+      <div className="team-color-container" style={containerStyle}>
         <img src={teamBannerUrl} alt={teamBannerUrl} />
       </div>
     )
   }
 
   render() {
-    return (
-      <div>
-        <h1>hii</h1>
-      </div>
-    )
+    return this.renderTeamsMatches()
   }
 }
 export default TeamMatches
